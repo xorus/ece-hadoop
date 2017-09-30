0d519db8b695ed92e6ca8dcc1ad387126f28d74c
Josua Gonzalez <jdpepi@gmail.com>
2017-09-30 12:29:01 +0200

    test implementation

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Mapper {
+
+  Map<> map(Map<String, String> input) {
+    Map<Integer, String[]> newMap = new HashMap<Integer, String>();
+    for (String str: input) {
+      String[] tab = input.split(',');
+      for (String value: tab) {
+        String[] vals = newMap.get(
+      }
+    }
+  }
+
+}
+
+
+
